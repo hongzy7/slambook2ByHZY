7324b056619f8e4d9f2ffd39ce61f5bdae024eb5
hongzy7 <hongzy7@mail3.sysu.edu.cn>
2023-09-11 10:51:40 +0800

    :construction:

--- build/ch3/cmake_install.cmake
+++ build/ch3/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump-14")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
